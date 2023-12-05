11aa6e614ca45450f3f26848c033eac549ef5185
Eduardo Reis <eduardorprincepe007@gmail.com>
2023-12-05 16:24:54 -0300

    js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
 }
-    document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap (){
-    document.querySelector('#som_tecla_clap').play();
-}
-    document.querySelector('.tecla_clap').onclick = tocaSomClap;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+    listaDeTeclas[0].onclick = tocaSomPom
