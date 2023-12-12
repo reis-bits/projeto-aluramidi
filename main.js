8b33fbdbddb21a93f1d6cb99ebf2042530b9bc06
Eduardo Reis <eduardorprincepe007@gmail.com>
2023-12-12 16:47:48 -0300

    js

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ function tocaSomPom (){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-    listaDeTeclas[0].onclick = tocaSomPom
+while () {
+        listaDeTeclas[0].onclick = tocaSomPom
+}
