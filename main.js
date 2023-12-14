fb0388cda8cb2232645213a24123416327018797
Eduardo Reis <eduardorprincepe007@gmail.com>
2023-12-14 15:33:48 -0300

    javascript otimizado
    Fiz as otimizaões do main.js do alura midi pra ficar super funcional!

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio){
+    
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,7 +9,17 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio)
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
     contador = contador + 1;
     
