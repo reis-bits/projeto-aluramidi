3eebad02602e2abcc9653d3e2395ea27be46cf62
Eduardo Reis <eduardorprincepe007@gmail.com>
2023-12-23 14:57:07 -0300

    finalização projeto alura midi

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function tocaSom (idElementoAudio){
-    
-    document.querySelector(idElementoAudio).play();
+function tocaSom (selTagAudio){
+    const elemento = document.querySelector(selTagAudio);
+    if (elemento != null && elemento.localName === 'audio') {
+            elemento.play();
+            console.log(elemento);
+    }
+    else {
+        console.log('Elemento não encontrado, ou seletor inválido')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,5 +19,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
